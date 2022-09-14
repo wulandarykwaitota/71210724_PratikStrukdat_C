38a81ce94e929df5a890040cdcd77ac3310eb136
wulandarykwaitota <93325366+wulandarykwaitota@users.noreply.github.com>
2022-09-14 19:06:57 +0700

    Add files via upload

---
+++ 71210724_UG3_C.py
@@ -0,0 +1,12 @@
+facto = int(input("Masukan range data: "))
+kamus = dict()
+
+for x in range(range):
+    if x % 2 ==0:
+        n = x
+        fact = 1
+    else:
+                
+        
+        kamus[x] = fa
+print(kamus)
